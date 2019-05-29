2676333d219ac782bd7fb9d8b4feef0d42a4841e
Karol-Perec <karol.perec@gmail.com>
2019-05-30 01:09:11 +0200

    update przed pullem klasy

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -130,7 +130,10 @@ void handleLogin() {                         // If a POST request is made to URI
 	}
 }
 
-
 void handleNotFound() {
 	server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void handleAlarmSetter() {
+
 }
