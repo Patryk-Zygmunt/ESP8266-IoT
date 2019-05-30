104c4d9ee44cbde4b308c85773dc6a982aaae4e3
Karol-Perec <karol.perec@gmail.com>
2019-05-30 02:43:51 +0200

    Po problemach

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -53,17 +53,14 @@ void handleRoot();              // function prototypes for HTTP handlers
 void handleLogin();
 void handleNotFound();
 
-//const int led = 2;
 
 void setup(void) {
 	Serial.begin(115200);         // Start the Serial communication to send messages to the computer
 	delay(10);
 	Serial.println('\n');
 
-////laczenie z WiFi/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////laczenie z WiFi/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	wifiMulti.addAP("xD", "12345678");   // add Wi-Fi networks you want to connect to
-	//wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
-	//wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
 
 	Serial.println("Connecting ...");
 	int i = 0;
@@ -76,20 +73,23 @@ void setup(void) {
 	Serial.println(WiFi.SSID());               // Tell us what network we're connected to
 	Serial.print("IP address:\t");
 	Serial.println(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
-	
+
 
 ////DNS -> http:// esp8266.local/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	if (MDNS.begin("esp8266")){              // Start the mDNS responder for esp8266.local
-		Serial.println("mDNS responder started");}
+	if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+		Serial.println("mDNS responder started");
+	}
 	else {
-		Serial.println("Error setting up MDNS responder!");}
+		Serial.println("Error setting up MDNS responder!");
+	}
 
 
-////HTTP server/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////HTTP server/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
-	//server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
 	server.on("/LED", HTTP_POST, handleLED);  // Call the 'handleLED' function when a POST request is made to URI "/LED"
-	server.on("/alarm", HTTP_GET, handleAlarm);
+	//server.on("/alarm", HTTP_GET, handleAlarm);
+	server.on("/ALARM_ON", HTTP_GET, handleAlarm);
+	server.on("/ALARM_OFF", HTTP_GET, handleAlarm);
 	server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
 	server.begin();                            // Actually start the server
@@ -101,7 +101,7 @@ void loop(void) {
 }
 
 void handleRoot() {                         // When URI / is requested, send a web page with a button to toggle the LED
-	server.send(200, "text/html", "<form action=\"/LED\" method=\"POST\"><input type=\"submit\" value=\"Toggle LED\"></form>");
+	server.send(200, "text/html", "<form action=\"/LED\" method=\"POST\"><input type=\"submit\" value=\"Toggle LED\"></form><form action=\"/ALARM_ON\" method=\"POST\"><input type=\"submit\" value=\"Alarm ON\" /></form><form action=\"/ALARM_OFF\" method=\"POST\"><input type=\"submit\" value=\"Alarm OFF\" /></form><form>ALARM ???</form>");
 }
 
 void handleLED() {                          // If a POST request is made to URI /LED
@@ -112,22 +112,8 @@ void handleLED() {                          // If a POST request is made to URI 
 
 void handleAlarm() {
 	int sensorState = digitalRead(D0);
-	Alarm alarm; 
+	Alarm alarm;
 	alarm.setAlarm(sensorState);
-}
-
-void handleLogin() {                         // If a POST request is made to URI /login
-	if (!server.hasArg("username") || !server.hasArg("password")
-		|| server.arg("username") == NULL || server.arg("password") == NULL) { // If the POST request doesn't have username and password data
-		server.send(400, "text/plain", "400: Invalid Request");         // The request is invalid, so send HTTP status 400
-		return;
-	}
-	if (server.arg("username") == "John Doe" && server.arg("password") == "password123") { // If both the username and the password are correct
-		server.send(200, "text/html", "<h1>Welcome, " + server.arg("username") + "!</h1><p>Login successful</p>");
-	}
-	else {                                                                              // Username and password don't match
-		server.send(401, "text/plain", "401: Unauthorized");
-	}
 }
 
 void handleNotFound() {
