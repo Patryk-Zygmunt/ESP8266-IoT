d3c1ddeb58ae98dc53417469be634e9c3233487b
Arkadiusz Olesek <arkadiusz.olesek@gmail.com>
2019-06-13 03:00:42 +0200

    [frontend-integration] Scheduler GUI Integrated with Project1.ino and login, refactoring.

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -1,18 +1,31 @@
+#include "ArduinoJson.h"
+
+#include <string>   // std::string
+#include <iostream> // std::cout
+#include <sstream>  // std::ostringstream
+
 #include "GreetingProvider.h";
 #include "LoginPageProvider.h";
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <FS.h>
 
 const char *ssid = "AAAAAAAAAAAAAAAAAAAAAAAAAAa";
 const char *password = "password";
+
+const char *index_path = "/index.html";
+const char *js_path = "/main.js";
+const char *html = "text/html";
+const char *js = "application/javascript";
 
 ESP8266WebServer server(80);
 LoginPageProvider loginProvider;
 
 void setup()
 {
+  SPIFFS.begin();
   Serial.begin(115200);
   delay(2000);
   initializeWiFi();
@@ -20,6 +33,10 @@ void setup()
   size_t headerkeyssize = sizeof(headerkeys) / sizeof(char *);
   server.collectHeaders(headerkeys, headerkeyssize);
   server.on("/", handleRoot);
+  server.on("/main.js", handleJs);
+  server.on("/devices.html", handleJs);
+  server.on("/scheduler", HTTP_POST, handlePostScheduler);
+  server.on("/scheduler", HTTP_GET, handleGetScheduler);
   server.on("/loginPage", handleLoginPage);
   server.on("/doLogin", handleDoLogin);
   server.begin();
@@ -64,14 +81,97 @@ void logInfo()
   }
 }
 
+void handleJs()
+{
+  if (SPIFFS.exists(js_path))
+  {
+    File jsFile = SPIFFS.open(js_path, "r");
+    size_t sent = server.streamFile(jsFile, js);
+    jsFile.close();
+    Serial.printf("File %s sent, %d bytes\n", js_path, sent);
+  }
+  else
+  {
+    Serial.printf("%s does not exist\n", js_path);
+  }
+  handleGetScheduler();
+}
+
 void handleRoot()
 {
   if (!checkIfAuthorized())
   {
     return;
   }
-  std::string contents = "hello from esp8266! <br/>" + loginProvider.getLogoutButton();
-  server.send(200, "text/html", contents.c_str());
+
+  if (SPIFFS.exists(index_path))
+  {
+    File indexFile = SPIFFS.open(index_path, "r");
+    size_t sent = server.streamFile(indexFile, html);
+    indexFile.close();
+    Serial.printf("File %s sent, %d bytes\n", index_path, sent);
+  }
+  else
+  {
+    Serial.printf("%s does not exist\n", index_path);
+  }
+  // std::string contents = "hello from esp8266! <br/>" + loginProvider.getLogoutButton();
+  // server.send(200, "text/html", contents.c_str());
+}
+
+void handlePostScheduler()
+{
+  if (!checkIfAuthorized())
+  {
+    return;
+  }
+  Serial.printf("POST /scheduler");
+  StaticJsonDocument<256> doc;
+  deserializeJson(doc, server.arg("plain"));
+  JsonObject root = doc.as<JsonObject>();
+  server.send(200, "text/html", "OK");
+
+  // Examples
+  Serial.println(server.arg("plain"));
+  String pin = doc["pin"];
+  String thing = doc["thing"];
+  String date = doc["date"];
+  String action = doc["action"];
+  Serial.println(pin.c_str());
+  Serial.println(thing.c_str());
+  Serial.println(date.c_str());
+  Serial.println(action.c_str());
+}
+
+void handleGetScheduler()
+{
+  if (!checkIfAuthorized())
+  {
+    return;
+  }
+  Serial.printf("GET /scheduler");
+
+  StaticJsonDocument<400> doc;
+  JsonArray array = doc.to<JsonArray>();
+  JsonObject event1 = array.createNestedObject();
+  event1["pin"] = "1";
+  event1["thing"] = "thing_a";
+  event1["date"] = "12-12-12";
+  event1["action"] = "Turn on";
+  JsonObject event2 = array.createNestedObject();
+  event2["pin"] = "2";
+  event2["thing"] = "thing_b";
+  event2["date"] = "12-12-12";
+  event2["action"] = "Turn off";
+  JsonObject event3 = array.createNestedObject();
+  event3["pin"] = "3";
+  event3["thing"] = "thing_c";
+  event3["date"] = "12-12-12";
+  event3["action"] = "Turn on";
+
+  String myString;
+  serializeJson(array, myString);
+  server.send(200, "text/json", myString);
 }
 
 void handleLoginPage()
@@ -106,15 +206,15 @@ bool checkIfAuthorized()
   if (server.hasHeader("Cookie"))
   {
     std::string token = toStdStr(server.header("Cookie"));
-    Serial.printf("Trying to access page with token: %s", token.c_str());
+    Serial.printf("Trying to access page with token: %s\n", token.c_str());
     bool tokenValid = loginProvider.isTokenValid(token);
     if (tokenValid)
     {
-      Serial.printf("Access granted.");
+      Serial.printf("Access granted.\n");
       return true;
     }
   }
-  Serial.printf("Access denied!");
+  Serial.printf("Access denied!\n");
   server.send(403, "text/html", loginProvider.getInvalidCredentialsPage().c_str());
   return false;
 }
--- Project1/data/main.js
+++ Project1/data/main.js
@@ -1,7 +1,8 @@
 
-function addEvent(){
+function addEvent() {
     var event = Array.from($("#add-event").serializeArray())
-    axios.post(URL +'/scheduler',arrayToObject(event )).then(r=>insertTable(r.data))
+    console.log(event)
+    axios.post('/scheduler', arrayToObject(event)).then(r => insertTable(r.data))
 
 }
 
@@ -11,39 +12,40 @@ const arrayToObject = (array) =>
         return obj
     }, {})
 
-
 function insertTable(arr) {
     const events = document.getElementById("events");
     events.innerHTML = ""
-    arr.map(res=> {
+    arr.map(res => {
         return {
             thing: res.thing,
             pin: res.pin,
-            date: moment(res.date, 'DD-MM-YYYY')
+            date: moment(res.date, 'DD-MM-YYYY'),
+            action: res.action
         }
     })
-        .sort((a,b)=>a.date.valueOf()- b.date.valueOf())
-        .forEach((item,index)=>events.appendChild(createTableRow(item.thing,item.pin,item.date,index)))
+        .sort((a, b) => a.date.valueOf() - b.date.valueOf())
+        .forEach((item, index) => events.appendChild(createTableRow(item.thing, item.pin, item.date, item.action, index)))
 };
 
-
-
-const deleteEvent = (name,date)=>[
-
-    axios.delete(URL +'/scheduler',arrayToObject(event )).then(r=>insertTable(r.data))
-]
-
-function createTableRow(name,pin,date,index) {
+function createTableRow(name, pin, d, action, index) {
     var frag = document.createDocumentFragment(),
         temp = document.createElement('tr');
     temp.innerHTML =
         `<th scope='row'>${index}</th>
             <td>${name}</td>
             <td>${pin}</td>
-            <td>${date.format('DD-MM-YYYY').toLocaleString()}</td>
-            <td><button type="button" class="btn btn-danger" onclick="deleteEvent('${name}','${pin}')">Delete</button></td>
-`
+            <td>${(d.format()).toLocaleString()}</td>
+            <td><button type="button" class="btn btn-secondary" onclick="deleteEvent('${name}','${pin}')">${action}</button></td>
+    `
     frag.appendChild(temp)
     return frag;
 }
+
+const makeAction = (pin) => [
+    axios.get('/scheduler/pin').then(r => console.log(r))
+]
+
+
+
+
 
